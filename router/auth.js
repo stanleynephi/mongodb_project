b6b6ab9be875639fd8e74bbfdee7bf539e562e8c
stanleynephi <131176126+stanleynephi@users.noreply.github.com>
2025-08-22 11:13:27 +0000

    Update auth.js

--- router/auth.js
+++ router/auth.js
@@ -16,7 +16,7 @@ passport.use(
       userInfoURL: "https://" + process.env["AUTH0_DOMAIN"] + "/userinfo",
       clientID: process.env["AUTH0_CLIENT_ID"],
       clientSecret: process.env["AUTH0_CLIENT_SECRET"],
-      callbackURL: "/oauth2/redirect",
+      callbackURL: "https://mongodb-project-3qz4.onrender.com/oauth2/redirect",
       scope: ["profile"],
     },
     function verify(issuer, profile, cb) {
